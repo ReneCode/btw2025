ea18b9fb50b24d44a69d020bb048427cdbf59b2d
Reinhard Langner <ReinhardLangner@gmx.de>
2025-02-22 17:10:49 +0100

    set env with next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    URL: process.env.URL || "http://localhost:3000",
+  },
 };
 
 export default nextConfig;
